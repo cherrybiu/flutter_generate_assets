a1db71d548532c92bc4d6ce06fc01c78964b8f92
淡航 <m15274906257@163.com>
2021-05-27 18:49:04 +0800

    feat: 格式化生成代码

--- lib/format.dart
+++ lib/format.dart
@@ -1,6 +1,6 @@
 import 'package:dart_style/dart_style.dart';
 
 String formatFile(String source) {
-  final DartFormatter df = DartFormatter();
+  final DartFormatter df = DartFormatter(pageWidth: 120);
   return df.format(source);
 }
