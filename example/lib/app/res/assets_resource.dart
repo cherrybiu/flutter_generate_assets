616d5754131583cd3c5fb4f8b7ffedbbd66c09fb
淡航 <m15274906257@163.com>
2021-05-27 18:48:05 +0800

    feat: font资源

---
+++ example/lib/app/res/assets_resource.dart
@@ -0,0 +1,20 @@
+/// Generate by [resource_generator](https://github.com/cherrybiu/flutter_generate_assets) library.
+
+class AssetsUtil {
+  const AssetsUtil._();
+  static const String address = 'assets/address.png';
+  static const String audio = 'assets/audio.png';
+  static const String bluetooth_link = 'assets/bluetooth_link.png';
+  static const String bluetoothoff = 'assets/bluetoothoff.png';
+  static const String bluetoothon_fjdfj = 'assets/bluetoothon-fjdfj.png';
+  static const String iconfont_ttf = 'fonts/iconfont.ttf';
+  static const String sm_nf_ttf = 'fonts/sm_nf.ttf';
+  static const String address_space = 'images/address space.png';
+  static const String addto = 'images/addto.png';
+  static const String bluetoothon = 'images/bluetoothon.png';
+  static const String camera = 'images/camera.png';
+  static const String child = 'images/child.png';
+  static const String course = 'images/course.png';
+  static const String pic_address_at_at = 'images/pic/address@at.png';
+  static const String pic_address_test = 'images/pic/address_test.png';
+}
